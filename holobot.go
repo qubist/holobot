5cb6f39cbdd087bcb278df99b3b08c097bc2bda2
Will Harris-Braun <willharrisbraun@gmail.com>
2018-03-17 20:08:23 -0400

    Recognize commands with multiple spaces before them
    This makes it so that `@holobot  time`, as well as `@holobot   time`
    will be recognized. I made it work this way because that's how it works
    in bash.

--- holobot.go
+++ holobot.go
@@ -607,8 +607,7 @@ func HandleCommands(event *model.WebSocketEvent) (err error) {
 		// ignore anything that doesn't say @holobot
 		if matched, _ := regexp.MatchString(`(?:^|\W)@`+config.UserName+`(?:$|\W)`, post.Message); matched {
 			for _, cmd := range commands {
-				// help command
-				if matched, _ := regexp.MatchString(`(?:^|\W)@`+config.UserName+` `+cmd.Name+`(?:$|\W)`, post.Message); matched {
+				if matched, _ := regexp.MatchString(`(?:^|\W)@`+config.UserName+` +`+cmd.Name+`(?:$|\W)`, post.Message); matched {
 					cmd.Handler(event, post)
 				}
 			}
